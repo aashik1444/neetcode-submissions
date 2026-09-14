ed80b2fa384bae490278bb9030375e92c5b19e85
Aashik Sandeep <aashiksandeep4@gmail.com>
2026-09-14 18:37:17 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        if (strs.size() == 0) {
+            return Character.toString((char)258);
+        }
+        String separate = Character.toString((char)257);
+        StringBuilder sb = new StringBuilder(); //to make string mutable
+        for (String s:strs) {
+            sb.append(s);
+            sb.append(separate);
+        }//[cat#dog#mouse#]
+        sb.deleteCharAt(sb.length()-1); //[cat#dog#mouse]
+        return sb.toString(); //converts string builder to normal string
+
+    }
+
+    public List<String> decode(String str) {
+        if (str.equals(Character.toString((char)258))) return new ArrayList();
+
+        String separate = Character.toString((char)257);
+
+        return Arrays.asList(str.split(separate, -1));
+
+    }
+}
