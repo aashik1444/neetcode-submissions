4b61ce1796c7ffcf40231f4e3448a852c2db545c
Aashik Sandeep <aashiksandeep4@gmail.com>
2026-09-23 19:58:30 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> hm = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+        hm.put(')','(');
+        hm.put('}','{');
+        hm.put(']','[');
+        for (char c : s.toCharArray()) {
+            if (hm.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == hm.get(c)) {
+                    stack.pop();
+                } else return false;
+            } else stack.push(c);
+        }
+        return stack.isEmpty();
+        
+    }
+}
