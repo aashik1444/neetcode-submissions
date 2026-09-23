25a0051be225cb82cc738ef94cc8aaab33627393
Aashik Sandeep <aashiksandeep4@gmail.com>
2026-09-23 20:17:36 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,28 @@
+class MinStack {
+    Stack<Integer> s;
+    Stack<Integer> ms;
+
+    public MinStack() {
+        s = new Stack<>();
+        ms = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s.push(val);
+        if (ms.isEmpty() || val <= ms.peek()) ms.push(val);
+    }
+    
+    public void pop() {
+        if (s.isEmpty()) return;
+        int t = s.pop();
+        if (t == ms.peek()) ms.pop(); 
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        return ms.peek();
+    }
+}
